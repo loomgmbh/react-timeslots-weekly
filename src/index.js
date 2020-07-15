2063c7dbdc68ddbcce81f3115db9610bf4ff17c7
Isaac Bigsby Trogdon <i.trogdon@loom.de>
2020-07-15 13:44:53 +0200

    Refs #24647, react module init.

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const Root = () => {return (
+  <>Hi there</>
+)}
+
+render(<Root/>, document.querySelector('#product-booking-calendar-target'));
